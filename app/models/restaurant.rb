c4ae27396b8be3f05ef8dfba113a5067136aeb40
romain isoz <isozromain@gmail.com>
2023-02-22 13:45:42 +0100

    ajout de toutes les fonctions liées aux restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,12 @@
 class Restaurant < ApplicationRecord
+  def self.categories
+    %w[chinese italian japanese french belgian]
+  end
+
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :category, inclusion: { in: Restaurant.categories }
 end
