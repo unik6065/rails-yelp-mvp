21070c54a5a697ada8eb0dbf567cc663f28d4056
romain isoz <isozromain@gmail.com>
2023-02-22 10:04:59 +0100

    ajout du fichier de seed pour les restaurants, et peuplage de ma db

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, inclusion: { in: (0..5) }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Faker::Config.locale = 'fr-CH'
+puts 'clean the database'
+Restaurant.destroy_all
+puts 'start the creation of the restaurant'
+25.times do
+  Restaurant.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
+puts 'finish the creation of the restaurant'
