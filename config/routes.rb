bbddfce20d66b9477494f089598080e2aebd33a3
romain isoz <isozromain@gmail.com>
2023-02-23 07:59:17 +0100

    exercice partiellement fini, à améliorer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: %i[index show new create]
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,21 @@ require 'faker'
 Faker::Config.locale = 'fr-CH'
 puts 'clean the database'
 Restaurant.destroy_all
-puts 'start the creation of the restaurant'
+Review.destroy_all
+puts 'start the creation of the restaurant with reviews'
 25.times do
-  Restaurant.create!(
+  restaurant = Restaurant.create!(
     name: Faker::Company.name,
     address: Faker::Address.street_address,
     phone_number: Faker::PhoneNumber.phone_number_with_country_code,
     category: %w[chinese italian japanese french belgian].sample
   )
+  rand(1..6).times do
+    Review.create!(
+      content: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4),
+      rating: rand(5),
+      restaurant_id: restaurant.id
+    )
+  end
 end
 puts 'finish the creation of the restaurant'
